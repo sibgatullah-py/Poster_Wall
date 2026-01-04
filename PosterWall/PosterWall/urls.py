1419ca3ee73937b90f92b72e436d4cbeb1ef99f7
sibgatullah-dev <sibgatullah881@gmail.com>
2026-01-04 21:10:32 +0600

    models and register Tweet model

--- PosterWall/PosterWall/urls.py
+++ PosterWall/PosterWall/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/', include('tweet.urls')),
+    path('', include('tweet.urls')),
     
     path("__reload__/", include("django_browser_reload.urls")),
     
--- PosterWall/tweet/models.py
+++ PosterWall/tweet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=500)
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)# this will kick in when the post will be added
+    updated_at = models.DateTimeField(auto_now=True)# this will kick in when the post gets updated 
+    
+    def __str__(self):
+        return f'{self.user.username} - {self.text[:15]}'
